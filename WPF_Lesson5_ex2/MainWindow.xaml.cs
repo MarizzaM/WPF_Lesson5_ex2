72b470e64cb298dee9a644210f6002852fc1f3ae
Pavlova Marina <marizza.mil89@gmail.com>
2021-04-04 21:28:34 +0300

    Add project files.

---
+++ WPF_Lesson5_ex2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Lesson5_ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    SafeInvoker.SafeInvoke(this, ChangeColor);
+                    Thread.Sleep(1);
+                }
+            });
+
+        }
+        private void ChangeColor()
+        {
+            Random random = new Random();
+
+            List<SolidColorBrush> brushes = new List<SolidColorBrush>();
+            brushes.Add(Brushes.AliceBlue);
+            brushes.Add(Brushes.MediumSpringGreen);
+            brushes.Add(Brushes.Violet);
+            brushes.Add(Brushes.HotPink);
+            brushes.Add(Brushes.Fuchsia);
+            brushes.Add(Brushes.Gold);
+            brushes.Add(Brushes.YellowGreen);
+            brushes.Add(Brushes.DarkViolet);
+            brushes.Add(Brushes.LightPink);
+            brushes.Add(Brushes.BurlyWood);
+
+            brdr1.BorderBrush = brushes[random.Next(10)];
+            brdr2.BorderBrush = brushes[random.Next(10)];
+            brdr3.BorderBrush = brushes[random.Next(10)];
+        }
+
+        private void SafeInvoke(Action work)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                work.Invoke();
+                return;
+            }
+            this.Dispatcher.BeginInvoke(work);
+        }
+    }
+}
---
+++ WPF_Lesson5_ex2/SafeInvoker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Lesson5_ex2
+{
+    public class SafeInvoker
+    {
+        public static void SafeInvoke(Window window, Action uiWork)
+        {
+            // check the current hread.
+            // if its UI thead then execute uiWork
+            // if not, then summon dispatcher.invoke (uiWork)
+            if (window.Dispatcher.CheckAccess()) // am I the UI thread?
+            {
+                uiWork.Invoke();
+                return;
+            }
+            window.Dispatcher.BeginInvoke(uiWork); // calls the UI thread to draw!
+        }
+
+    }
+}
